320140bb0ab935b222089e04ea2cb718ed074cb1
CaoCong <caocong2404@gmail.com>
2023-06-11 11:42:04 +0700

    Setting account login with email or username, design email sending, css contact page, add navbar

--- RecipeOrganizerASP-master/RecipeOrganizer/Program.cs
+++ RecipeOrganizerASP-master/RecipeOrganizer/Program.cs
@@ -46,8 +46,8 @@ builder.Services.Configure<IdentityOptions>(options =>
     options.Password.RequiredUniqueChars = 1; // Số ký tự riêng biệt
 
     // Cấu hình Lockout - khóa user
-    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5); // Khóa 5 phút
-    options.Lockout.MaxFailedAccessAttempts = 5; // Thất bại 5 lầ thì khóa
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(2); // Khóa 5 phút
+    options.Lockout.MaxFailedAccessAttempts = 7; // Thất bại 7 lầ thì khóa
     options.Lockout.AllowedForNewUsers = true;
 
     // Cấu hình về User.
