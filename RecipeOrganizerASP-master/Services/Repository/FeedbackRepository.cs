2a926bc3dcf85aee7ede9247becfd3c898a64f3b
dangkhoaaaa <dangkhoa3348@gmail.com>
2023-06-20 09:45:24 +0700

    update search title; recipe function

--- RecipeOrganizerASP-master/Services/Repository/FeedbackRepository.cs
+++ RecipeOrganizerASP-master/Services/Repository/FeedbackRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Services.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,41 @@ namespace Services.Repository
 {
     public class FeedbackRepository : RepositoryBase<FeedbackRepository>
     {
+        Recipe_OrganizerContext _context;
+
+        protected DbSet<Feedback> _dbSetFeedBack;
+        protected DbSet<Metadata> _dbSetMetadata;
+
+        public FeedbackRepository()
+        {
+            _context = new Recipe_OrganizerContext();
+            _dbSetFeedBack = _context.Set<Feedback>();
+            _dbSetMetadata = _context.Set<Metadata>();
+        }
+        public double valueAvgRateRecipe(int recipeId)
+        {
+            List<Feedback> listmapCategory = new List<Feedback>();
+            var query = from fb in _dbSetFeedBack
+                        join md in _dbSetMetadata on fb.FeedbackId equals md.FeedbackId
+                        where md.RecipeId == recipeId
+                        select fb;
+
+            listmapCategory = query.DistinctBy( r => r.FeedbackId).ToList();
+            double result = 0;
+            if (listmapCategory.Count > 0 )
+            {
+
+                double totalRate = 0;
+                foreach (var item in listmapCategory)
+                {
+                    totalRate += item.Rating;
+                }
+
+                 result = totalRate / listmapCategory.Count;
+            }
+           
+
+            return result;
+        }
     }
 }
