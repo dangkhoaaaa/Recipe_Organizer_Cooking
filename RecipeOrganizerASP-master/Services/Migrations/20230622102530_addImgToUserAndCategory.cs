1d8cadb59ecbd218dc936d1ec2c79c1596df07be
caocong2404 <caocong2404@gmail.com>
2023-06-22 17:26:33 +0700

    Register with firstname & lastname,. Add img field to user and category

---
+++ RecipeOrganizerASP-master/Services/Migrations/20230622102530_addImgToUserAndCategory.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Services.Migrations
+{
+    public partial class addImgToUserAndCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "image",
+                table: "Category",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "image",
+                table: "Category");
+
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "AspNetUsers");
+        }
+    }
+}
