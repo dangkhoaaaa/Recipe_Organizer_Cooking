bddaad179bb4c55b21660c69504ed50424b1d1d9
dangkhoaaaa <dangkhoa3348@gmail.com>
2023-06-28 21:12:56 +0700

    dashbroad commit

--- RecipeOrganizerASP-master/RecipeOrganizer/Areas/Admin/Models/CategoryViewModel.cs
+++ RecipeOrganizerASP-master/RecipeOrganizer/Areas/Admin/Models/CategoryViewModel.cs
@@ -5,6 +5,8 @@
 
         public int category_id { get; set; }
         public int parent_id { get; set; }
+
+        public string? IMG { get; set; }
         public string? title { get; set; }
         public string? description { get; set; }
         public string? parent_name { get; set; }
