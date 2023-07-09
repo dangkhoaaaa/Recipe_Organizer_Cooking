fb7436b5a92943ad91aed88e053e7ce844c5ba6d
k16 phideptraivodichvutru <phidepzai02@gmail.com>
2023-07-09 20:14:33 +0700

    updating contact

--- RecipeOrganizerASP-master/RecipeOrganizer/Controllers/ContactController.cs
+++ RecipeOrganizerASP-master/RecipeOrganizer/Controllers/ContactController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Services.Repository;
 
 namespace RecipeOrganizer.Controllers
 {
@@ -8,6 +9,25 @@ namespace RecipeOrganizer.Controllers
 		public IActionResult SendContact()
 		{
 			return View();
+		}
+		[HttpPost]
+		public ActionResult SendEmail(ContactFormModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				// Access the form data from the model
+				string name = model.Name;
+				string email = model.Email;
+				string address = model.Address;
+				string message = model.Message;
+
+				// Send email using your preferred email sending method or library
+
+				return RedirectToAction("Success"); // Redirect to a success page
+			}
+
+			// If the model state is not valid, return to the same view to show validation errors
+			return View("SendContact", model);
 		}
 	}
 }
---
+++ RecipeOrganizerASP-master/Services/Repository/ContactFormModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Repository
+{
+	public class ContactFormModel
+	{
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public string Address { get; set; }
+		public string Message { get; set; }
+	}
+}
