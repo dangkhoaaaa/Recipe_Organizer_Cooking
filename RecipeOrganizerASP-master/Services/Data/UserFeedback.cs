04e21160dc6c4e28d83b1f52b17f6dbe41b445c2
caocong2404 <caocong2404@gmail.com>
2023-07-10 20:26:29 +0700

    Feedback page fix error

---
+++ RecipeOrganizerASP-master/Services/Data/UserFeedback.cs
@@ -0,0 +1,17 @@
+﻿using Services.Models.Authentication;
+using Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Data
+{
+	public class UserFeedback
+	{
+		public AppUser User { get; set; }
+		public Recipe Recipe { get; set; }
+		public Feedback Feedback { get; set; }
+	}
+}
